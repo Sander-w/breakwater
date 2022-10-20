65793a9d386bdf8bb8a52b669353f381c29be7b9
lkroon <luckroon92@gmail.com>
2022-10-20 15:14:06 +0200

    intermediate script

---
+++ Notebooks/Constanta Phase III/Constanta Phase III load data_toe_scour.py
@@ -0,0 +1,238 @@
+# %%
+import breakwater as bw
+import pandas as pd
+import os
+from pathlib import Path
+import numpy as np
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.info("Initiated script")
+
+# %%
+from development_overtopping_DKA import (eurotop2018_6_5, 
+                                         surf_similarity, 
+                                         gamma_beta_eurotop_2018_6_9, 
+                                         calc_beta)
+from development_armour_stability_DKA import (vangent_armour_reduction, 
+                                              hudson_fixed_slope, 
+                                              rock_manual_5_196, 
+                                              rock_manual_5_195, 
+                                              rock_manual_5_194)
+from development_material_DKA import(get_class,
+                                     get_Dn50)
+
+from development_scour_DKA import(sumer_fredsoe,
+                                 v_scour)
+
+
+from breakwater.utils.exceptions import user_warning
+
+# %%
+project_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx",
+                             index_col = 1,
+                             sheet_name='Input_Project specific')
+requirements_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx",
+                                  index_col = 0,
+                                  sheet_name='Input_requirements')
+wave_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx",
+                          index_col = 0,
+                          sheet_name='input_hydrotechnical',
+                         skiprows = 1)
+wave_data["Location"] = wave_data["Structure"] + wave_data["Chainage"]
+cross_section_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx", 
+                                   sheet_name='Input_Cross section',
+                                  skiprows = 1)
+cross_section_data["Location"] = cross_section_data["Structure"] + cross_section_data["Chainage"]
+cross_section_data = cross_section_data.set_index('Location')                           
+concrete_element_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx", 
+                                      sheet_name='Input_concrete_elements',
+                                      index_col = 0,
+                                      skiprows = 1)
+gradings_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx", 
+                                      sheet_name='input_rock_gradings',
+                                      index_col = 0,
+                                      skiprows = 2)
+
+# %%
+# CALCULATE TOE STABILITY
+
+Nod_allowed_list = []
+Delta_list = []
+Hs_list = []
+h_list = []
+t_filter_list = []
+toe_layer_thickness_list = []
+ht_list = []
+Dn50_toe_list = []
+toe_armour_class_list = []
+Dn50_toe_temp_list = []
+
+for Calculation_case in range(1, len(wave_data.index)+1):
+    # Calculation_case = 1
+    Cross_section_id = wave_data.at[Calculation_case, 'Location']
+
+    # Open project specific parameters
+    g            = project_data.at['g'           , 'Value']
+    rho_a        = project_data.at['rho_r'       , 'Value']
+    rho_w        = project_data.at['rho_w'       , 'Value']
+    Lt           = project_data.at['Lt'          , 'Value']
+    t_underlayer = project_data.at['t_underlayer', 'Value']
+
+    #Get info for sea state
+    Hm0      = wave_data.at[Calculation_case, 'Hm0']
+    wl       = wave_data.at[Calculation_case, 'wl']
+
+    # Open structure specific parameters
+    z_bed           = cross_section_data.at[Cross_section_id, 'z_bed']
+
+    # Get allowed Sd for the location and calculation case
+    LS          = wave_data.at[Calculation_case, 'Limit State']
+    Nod_allowed = requirements_data.at['Toe damage limit', LS]
+
+    # Intermediate parameters
+    h               = wl-z_bed
+    Delta          = (rho_a-rho_w)/rho_w
+
+    #TEMPORARY FIX TO BE IN LINE WITH PHASE II
+    Hs = Hm0
+
+    # Calculate required stone size for toe
+
+    # use while loop since ht depends on Dn50 of the toe
+    # first set temporary values for the while loop
+    Dn50_toe = 0
+    Dn50_toe_temp = 0
+    compute_toe = True
+    counter = 0
+    t_filter = 0
+
+    toe_layer_thickness = t_underlayer + t_filter + Dn50_toe*Lt
+    ht = h - toe_layer_thickness
+
+    while compute_toe:
+        Dn50_toe_computed = bw.core.toe_stability(Hs, 
+                                                h, 
+                                                ht, 
+                                                Delta, 
+                                                Nod_allowed)    
+
+        # check for convergence
+        if Dn50_toe_computed - Dn50_toe_temp < 0 or counter > 50:
+            # value has converged, so break loop
+            compute_toe = False
+
+        counter += 1
+        
+        # check if computed Dn50 is larger than current normative Dn50
+        if Dn50_toe_computed > Dn50_toe:
+            # if larger the normative Dn50 must be changed
+            Dn50_toe = Dn50_toe_computed
+            
+        # Set Dn50 toe class average
+        class_toe = get_class(Dn50_toe_computed, rho_a, gradings_data)
+        Dn50_toe_computed = get_Dn50(class_toe, gradings_data, 'Av')
+        
+        # Check if filter layer needs to be applied
+        if gradings_data.at[class_toe, 'Toe underlayer'] in gradings_data.index:
+            class_filter = gradings_data.at[class_toe, 'Toe underlayer']
+            Dn50_filter = get_Dn50(class_filter, gradings_data)
+            t_filter = Dn50_filter*Lt
+        
+        # replace old values with the new ones
+        Dn50_toe_temp = Dn50_toe_computed
+        toe_layer_thickness = t_underlayer + t_filter + Dn50_toe_temp*Lt
+        ht = h - toe_layer_thickness
+        
+    toe_armour_class = get_class(Dn50_toe, rho_a, gradings_data)
+
+    Nod_allowed_list.append(Nod_allowed)
+    Delta_list.append(Delta)
+    Hs_list.append(Hs)
+    h_list.append(h)
+    t_filter_list.append(t_filter)
+    toe_layer_thickness_list.append(toe_layer_thickness)
+    ht_list.append(ht)
+    Dn50_toe_list.append(Dn50_toe)
+    toe_armour_class_list.append(toe_armour_class)
+    Dn50_toe_temp_list.append(Dn50_toe_temp)
+
+wave_data["Nod_allowed"]= Nod_allowed_list
+wave_data["Delta"]= Delta_list
+wave_data["Hs"]= Hs_list
+wave_data["h"]= h_list
+wave_data["t_filter"]= t_filter_list
+wave_data["toe_layer_thickness"]= toe_layer_thickness_list
+wave_data["ht"]= ht_list
+wave_data["Dn50_toe"]= Dn50_toe_list
+wave_data["toe_armour_class"]= toe_armour_class_list
+wave_data["Dn50_toe_temp"]= Dn50_toe_temp_list
+
+wave_data.to_excel("wave_data_intermediate_toe_stability.xlsx")
+
+logging.info("Finished intermediate section")
+
+results = []
+for location in wave_data.Location.unique():
+    print(location)
+
+
+
+# %%
+# CALCULATE SCOUR DEPTH
+
+wave_data = pd.read_excel(Path("./Input data/") / "test_data_phase_II.xlsx",
+                          index_col = 0,
+                          sheet_name='input_hydrotechnical',
+                         skiprows = 1)
+wave_data["Location"] = wave_data["Structure"] + wave_data["Chainage"]
+
+Hs_list = []
+Tp_list = []
+h_list = []
+C2_sf_list = []
+S_list = []
+
+for Calculation_case in range(1, len(wave_data.index)+1):
+    # Calculation_case = 1
+    Cross_section_id = wave_data.at[Calculation_case, 'Location']
+
+
+    # Open project specific parameters
+    g            = project_data.at['g'           , 'Value']
+    C2_sf        = project_data.at['C2_sf'       , 'Value']
+
+    #Get info for sea state
+    Hm0      = wave_data.at[Calculation_case, 'Hm0']
+    Tp       = wave_data.at[Calculation_case, 'Tp']
+    wl       = wave_data.at[Calculation_case, 'wl']
+
+    # Open structure specific parameters
+    z_bed           = cross_section_data.at[Cross_section_id, 'z_bed']
+
+    # Intermediate parameters
+    h               = wl-z_bed
+
+    #TEMPORARY FIX TO BE IN LINE WITH PHASE II
+    Hs = Hm0
+
+    # Calculate scour depth
+    S = sumer_fredsoe(Hs, Tp, g, h, C2_sf)
+
+    Hs_list.append(Hs)
+    Tp_list.append(Tp)
+    h_list.append(h)
+    C2_sf_list.append(C2_sf)
+    S_list.append(S)
+
+wave_data['Hs'] = Hs_list
+wave_data['Tp'] = Tp_list
+wave_data['h'] = h_list
+wave_data['C2_sf'] = C2_sf_list
+wave_data['S'] = S_list
+
+wave_data.to_excel("wave_data_intermediate_scour.xlsx")
+
+
+
+
+
